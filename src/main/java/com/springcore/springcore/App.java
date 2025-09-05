05293f07b412115c296d29e4d6c8588b7cc090fe
Sumit kumar <sumitkumar808393@gmail.com>
2025-09-05 23:53:12 +0530

    Spring Exer

--- src/main/java/com/springcore/springcore/App.java
+++ src/main/java/com/springcore/springcore/App.java
@@ -1,5 +1,7 @@
 package com.springcore.springcore;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 /**
  * Hello world!
  *
@@ -9,5 +11,8 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+         ApplicationContext context = new ClassPathXmlApplicationContext("config.xml");
+         Student student1 = (Student) context.getBean("student1");
+         System.out.println(student1);
     }
 }
