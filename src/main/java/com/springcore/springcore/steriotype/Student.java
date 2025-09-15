a6155b2c7325de989c6a80259dbc6ce6b57ac66a
Sumit kumar <sumitkumar808393@gmail.com>
2025-09-15 22:05:44 +0530

    Steriotype Component and Value

---
+++ src/main/java/com/springcore/springcore/steriotype/Student.java
@@ -0,0 +1,30 @@
+package com.springcore.springcore.steriotype;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Student {
+	    @Value("Sumit")
+		private String studentName;
+	    @Value("Bihar")
+		private String city;
+		public String getStudentName() {
+			return studentName;
+		}
+		public void setStudentName(String studentName) {
+			this.studentName = studentName;
+		}
+		public String getCity() {
+			return city;
+		}
+		public void setCity(String city) {
+			this.city = city;
+		}
+		
+		@Override
+		public String toString() {
+			return "Student [studentName=" + studentName + ", city=" + city + "]";
+		}
+
+}
